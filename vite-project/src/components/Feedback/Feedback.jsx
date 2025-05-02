2ceb2865d4b630bf8032be3e1cd0d843a50cafe5
dilaraydmr <dilaraaydemir1@gmail.com>
2025-05-02 22:01:48 +0300

    commit msg

---
+++ vite-project/src/components/Feedback/Feedback.jsx
@@ -0,0 +1,10 @@
+function Feedback() {
+    return (
+        <div>
+            <h2>Feedback</h2>
+            {/* Geri bildirim sayıları buraya gelecek */}
+        </div>
+    );
+}
+
+export default Feedback;
